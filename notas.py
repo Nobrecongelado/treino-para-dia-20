de59793ec159dada932d6a9205e78b448030de7a
brayan <brayannunesnobre11@gmail.com>
2025-03-15 12:16:48 -0300

    media

---
+++ notas.py
@@ -0,0 +1,10 @@
+nota1=float (input("digita sua nota 1º bimestre: "))
+nota2=float (input("digita sua nota 2º bimestre: "))
+nota3=float (input("digita sua nota 3º bimestre: "))
+nota4=float (input("digita sua nota 4º bimestre: "))
+result=float=nota1+nota2+nota3+nota4
+
+print(result /4)
+media=float=(result /4)
+
+print("ola clique, sua nota é:" ,media)
