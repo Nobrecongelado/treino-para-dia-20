ec60517e278588c6dae327de84d952cf248a3c5b
brayan <brayannunesnobre11@gmail.com>
2025-03-15 12:26:10 -0300

    misterio

---
+++ misterioso.py
@@ -0,0 +1,18 @@
+tuple=( 2050, 15, 5200, 10400, 1)
+
+lista=list(tuple)
+
+
+
+lista.append( 41 )
+lista.append( 90 )
+
+
+del lista[0]
+del lista[5]
+
+
+dicionario=dict(nome="brayan", idade=19, profissão="imprensario")
+
+print(lista)
+print(dicionario)
